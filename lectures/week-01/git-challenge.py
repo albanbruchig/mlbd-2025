90db14ac841addf23919a10db237b7eed2b28267
Filip Mikoviny <filip.mikoviny@epfl.ch>
2025-02-18 16:29:09 +0100

    challenge

--- lectures/week-01/git-challenge.py
+++ lectures/week-01/git-challenge.py
@@ -4,12 +4,11 @@
 def person_a():
     """Reads the data in data/school_performance.csv
     and returns a dataframe with the first 5,000 rows.
-asdasdads
     Returns:
     dataframe: containing first 5,000 rows of school_performace.csv
     """
     # Code goes over here.
-
+    
     
     raise NotImplementedError()
 
